3885c9fe7a66f46e1b75480e514b37622548fee8
Raj Mishra <raj212558@gmail.com>
2026-02-05 00:52:12 +0530

    create patient models

---
+++ Backend/src/models/patient.models.js
@@ -0,0 +1,81 @@
+import mongoose, { Schema } from "mongoose";
+
+const patientSchema = new Schema(
+    {
+        patientId: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+            unique: true,
+            index: true,
+        },
+        bloodGroup: {
+            type: String,
+            enum: ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"],
+        },
+        alergies: {
+            type: [String],
+            default: [],
+        },
+        chronicDiseases: {
+            type: [String],
+            default: [],
+        },
+        emergencyContact: {
+            name: String,
+            relationship: String,
+            phone: String,
+        },
+
+        // Medical History
+        medicalHistory: [{
+            condition: String,
+            diagnosisDate: Date,
+            status: {
+                type: String,
+                enum: ["ACTIVE", "CHRONIC", "RESOLVED"],
+            },
+            notes: String,
+        }],
+
+        // Current Medications
+        medications: [{
+            name: String,
+            dosage: String,
+            frequency: String,
+            startDate: Date,
+            endDate: Date, 
+        }],
+
+        // Statistics
+
+        totalAppointments: {
+            type: Number,
+            default: 0,
+        },
+        completedAppointments: {
+            type: Number,
+            default: 0,
+        },
+        cancelledAppointments: {
+            type: Number,
+            default: 0,
+        },
+        followingDoctorsCount: {
+            type: Number,
+            default: 0,
+        },
+    },
+    {
+        timestamps: true,
+    }
+)
+
+// Inboxes
+patientSchema.index({ patientId: 1 });
+patientSchema.index({ bloodGroup: 1 });
+
+const Patient = mongoose.model("Patient", patientSchema);
+
+
+export { Patient };
