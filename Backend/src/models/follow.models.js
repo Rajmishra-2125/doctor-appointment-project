eaa0e25be2dcf358d121dc25ecc992cc3e65d4b6
Raj Mishra <raj212558@gmail.com>
2026-02-05 01:38:49 +0530

    update follow models

---
+++ Backend/src/models/follow.models.js
@@ -0,0 +1,52 @@
+import mongoose, { Schema} from "mongoose"
+
+const followSchema = new Schema(
+  {
+    doctorId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true,
+    },
+    patientId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true,
+    },
+
+    // Soft delete
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+    deletedAt: {
+      type: Date,
+      default: null,
+    },
+  },
+  { timestamps: true }
+);
+
+
+// Prevent duplicate follows
+
+doctorFollowSchema.index(
+    {
+        doctorId: 1,
+        patientId: 1,
+    },
+    {
+        unique: true
+    }
+)
+
+// Only fetch non-deleted follows by default
+doctorFollowSchema.pre(/^find/, function (next) {
+   if (!this.getOptions().includeDeleted) {
+       this.find({ isDeleted: false });
+   }
+   next();
+});
+
+export const Follow = mongoose.model("Follow", followSchema)
