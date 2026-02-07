9183985cea32bb1dc5ebbc3795771f14d703f5ca
Raj Mishra <raj212558@gmail.com>
2026-02-08 02:36:02 +0530

    Adding new routes

--- Backend/src/routes/appointment.routes.js
+++ Backend/src/routes/appointment.routes.js
@@ -1,19 +1,39 @@
 import { Router } from "express";
-import { getAvailableSlots, applyForBooking, cancelBooking } from "../controllers/appointment.controllers.js"
+import {
+  getAvailableSlots,
+  applyForBooking,
+  cancelBooking,
+  myAppointments,
+  getAppointmentDetailsBySlotId,
+  updateAppointmentStatus,
+  rescheduleAppointment,
+} from "../controllers/appointment.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
 const router = Router();
+
+// Get my appointments
+router.route("/").get(verifyJWT, myAppointments);
+
+// Get/Update specific appointment
+// Get Appointment details by Slot ID (New Route)
+router.route("/slot/:slotId").get(verifyJWT, getAppointmentDetailsBySlotId);
+
+// Update specific appointment (By Appointment ID)
+router.route("/:appointmentId").patch(verifyJWT, updateAppointmentStatus);
+
+// Reschedule
+router
+  .route("/:appointmentId/reschedule")
+  .patch(verifyJWT, rescheduleAppointment);
 
 // Checking availability by doctor username
 router.route("/checkslot").post(verifyJWT, getAvailableSlots);
 
 // Booking appointment
-
 router.route("/bookslot").post(verifyJWT, applyForBooking);
 
 // Cancelling appointment
-
 router.route("/cancelslot").post(verifyJWT, cancelBooking);
-
 
 export default router;
