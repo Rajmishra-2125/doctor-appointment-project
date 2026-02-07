9df643d7a864b6787833f7e8bd90e7f59a6d62d1
Raj Mishra <raj212558@gmail.com>
2026-02-08 02:37:03 +0530

    Adding new routes

--- Backend/src/routes/doctor.routes.js
+++ Backend/src/routes/doctor.routes.js
@@ -1,7 +1,6 @@
 import { Router } from "express";
 import {
   getDoctors,
-  getMyProfile,
   saveDoctorInfo,
   updateDoctorProfile,
   followDoctor,
@@ -13,7 +12,7 @@ import {
 } from "../controllers/doctor.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
-const router = Router()
+const router = Router();
 
 /**
  * =========================
@@ -21,40 +20,28 @@ const router = Router()
  * =========================
  */
 
-// Geting own profile
-router.route("/my-profile").get(verifyJWT, getMyProfile);
-
 // Saving info in doctor profile
-router.route("/saveInfo").post(verifyJWT, saveDoctorInfo)
-
+router.route("/saveInfo").post(verifyJWT, saveDoctorInfo);
 
 // Update doctor profile
-router.route("/updateInfo").patch(verifyJWT, updateDoctorProfile)
-
+router.route("/updateInfo").patch(verifyJWT, updateDoctorProfile);
 
 // Geting normal doctors list
 router.route("/profiles").get(verifyJWT, getDoctors);
 
-
 // follow the doctor profile
 router.route("/:username/follow").post(verifyJWT, followDoctor);
-
 
 // unfollow the doctor profile
 router.route("/:username/unfollow").delete(verifyJWT, unfollowDoctor);
 
-
 // Get doctors availability by Specilization
-router
-  .route("/:specialization")
-  .post(verifyJWT, getDoctorsBySpecialization);
-
+router.route("/:specialization").post(verifyJWT, getDoctorsBySpecialization);
 
 // Get doctors availability by Specilization and followers
 router
   .route("/:specialization/mostfollowers")
   .post(verifyJWT, getDoctorsByMostFollowers);
-
 
 // Geting availability
 router.route("/check-availability").get(verifyJWT, getAvailability);
@@ -62,7 +49,5 @@ router.route("/check-availability").get(verifyJWT, getAvailability);
 // Checking availabilty by doctor ID
 
 router.route("/:username/availability").get(verifyJWT, getAvailabilityById);
-
-
 
 export default router;
