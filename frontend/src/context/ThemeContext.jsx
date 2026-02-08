b7485ee457e2b0c4f0355c4a0037bbb5df5d1019
Raj Mishra <raj212558@gmail.com>
2026-02-08 10:11:28 +0530

    Creating context for Toggle button

---
+++ frontend/src/context/ThemeContext.jsx
@@ -0,0 +1,64 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState(
+    localStorage.getItem("theme") ? localStorage.getItem("theme") : "system"
+  );
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+
+    const removeOldTheme = () => {
+      root.classList.remove("dark");
+      root.classList.remove("light");
+    };
+
+    const applyTheme = (newTheme) => {
+      removeOldTheme();
+      if (newTheme === "dark") {
+        root.classList.add("dark");
+      } else if (newTheme === "light") {
+        root.classList.add("light");
+      } else if (newTheme === "system") {
+        const systemTheme = window.matchMedia("(prefers-color-scheme: dark)")
+          .matches
+          ? "dark"
+          : "light";
+        root.classList.add(systemTheme);
+      }
+    };
+
+    applyTheme(theme);
+
+    if (theme === "system") {
+      const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+      const handleChange = () => {
+        applyTheme("system");
+      };
+      mediaQuery.addEventListener("change", handleChange);
+      return () => mediaQuery.removeEventListener("change", handleChange);
+    }
+  }, [theme]);
+
+  // Save selection explicitly
+  const changeTheme = (newTheme) => {
+    setTheme(newTheme);
+    localStorage.setItem("theme", newTheme);
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, changeTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => {
+  const context = useContext(ThemeContext);
+  if (context === undefined) {
+    throw new Error("useTheme must be used within a ThemeProvider");
+  }
+  return context;
+};
