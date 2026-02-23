f70b6d6cbe49a5b440690db30c0aa23610752730
Raj Mishra <raj212558@gmail.com>
2026-02-23 20:16:53 +0530

    Updating our redux-store

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,9 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit';
-import authReducer from '../features/auth/authSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "../features/auth/authSlice";
+import doctorReducer from "../features/doctors/doctorSlice";
+import adminReducer from '../features/admin/AdminSlice'
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    doctor: doctorReducer,
+    admin: adminReducer,
   },
 });
 
