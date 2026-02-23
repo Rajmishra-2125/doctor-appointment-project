d82c7d5b0f0ee5f58d87d139b365ae7f4af66c99
Raj Mishra <raj212558@gmail.com>
2026-02-23 20:15:06 +0530

    Updating doctor Layout

--- frontend/src/pannel/Doctor/layout/DoctorLayout.jsx
+++ frontend/src/pannel/Doctor/layout/DoctorLayout.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header/Header.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+import Sidebar from "./components/Sidebar/Sidebar.jsx";
 
-function DoctorLayout() {
+function DoctorLayout({ children }) {
   return (
-    <div>
-      
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <div className="flex flex-1">
+        <Sidebar />
+        <main className="flex-1 bg-gray-50 flex flex-col">
+          <div className="flex-1 p-6">{children || <Outlet />}</div>
+          <Footer />
+        </main>
+      </div>
     </div>
-  )
+  );
 }
 
 export default DoctorLayout;
