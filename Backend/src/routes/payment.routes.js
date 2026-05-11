7f014f6b4549e887225589486d0fcef0696d0d28
Raj Mishra <raj212558@gmail.com>
2026-05-12 01:11:39 +0530

    Added Payment Routes

---
+++ Backend/src/routes/payment.routes.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
+import { createOrder, verifyPayment } from "../controllers/payment.controllers.js";
+
+const router = Router();
+
+// Secure all payment routes
+router.use(verifyJWT);
+
+router.post("/create-order", createOrder);
+router.post("/verify", verifyPayment);
+
+export default router;
